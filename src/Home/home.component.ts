bf49f5eb03097edd4333255fdbfb57a2806c0c78
AnuragPandey9 <32862773+AnuragPandey9@users.noreply.github.com>
2018-01-28 14:56:34 +0530

    updates only

--- src/Home/home.component.ts
+++ src/Home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
-import "rxjs/add/operator/retry
+
 
 @Component({
     selector: "Home",
@@ -14,7 +14,31 @@ export class HomeComponent {
     private anyErrors: boolean;
     private finished: boolean;
     private value:number=0;
-  //  private value:number=90;
     constructor() {
     }
+    // init() {
+    //     // this.data = new Observable(observer => {
+    //     //     setTimeout(() => {
+    //     //         observer.next('../assets/images/abc.webp');
+    //     //     }, 1000);
+            
+    //     //     setTimeout(() => {
+    //     //         observer.next('../assets/images/def.webp');
+    //     //     }, 3000);
+            
+    //     //     setTimeout(() => {
+    //     //         observer.complete();
+    //     //     }, 3000);
+    //     // });
+    //     // let subscription = this.data.subscribe(
+    //     //     value => this.values.push(value),
+    //     //     error => this.anyErrors = true,
+    //     //     () => this.finished = true
+    //     // );
+    //     // this.init(); 
+    //    setTimeout(()=>{
+    //          this.value = this.value+1;
+    //    },1000);
+    //   this.init(); 
+    // }
 }
